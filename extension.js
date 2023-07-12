4ce0e959930674ea1307e0c6932d7b7819e908fc
Madhav Anand <madhavanand402@gmail.com>
2023-07-12 15:47:43 +0530

    🚀  Completed

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const customParser = require('@madhavanand/object-string-to-object')
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,25 +11,34 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "clipboard-to-json" is now active!');
+	vscode.window.showInformationMessage('Activated 😀 : Clipboard to JSON ');
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('clipboard-to-json.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('clipboard-to-json.helloWorld', async () => {
+		const clipboardText = await vscode.env.clipboard.readText();
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from clipboard-to-json!');
+		try {
+			let finalString = customParser(clipboardText)
+			console.log(finalString);
+			finalString = JSON.stringify(finalString);
+			console.log(finalString);
+			await vscode.env.clipboard.writeText(finalString);
+			vscode.window.showInformationMessage('Done 🚀 : Check Clipboard');
+		} catch (error) {
+			vscode.window.showErrorMessage('Oops 😥 : Something Went Wrong');
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+	vscode.window.showInformationMessage('Deactivated ☹️ : Clipboard to JSON ');
+}
 
 module.exports = {
 	activate,
